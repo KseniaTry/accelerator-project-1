04c793d1b7ed6e650f7a0a785a651b6c389cdbf6
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-11-16 21:13:19 +0300

    feat: add faq block styles (mobile, tablet, desktop) + add switch tabs JS

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,43 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+// блок FAQ
+const faqButtonsContainer = document.querySelector(".faq__buttons-container");
+const faqTabContent = document.querySelector(".faq__tab-content");
+const faqTabItems = document.querySelectorAll(".faq__tab-item");
+
+// нажатие кнопок с выбором категории (центр, абонемент и тд)
+faqButtonsContainer.addEventListener('click', evt => {
+  const faqTabPaneId = evt.target.dataset.tab;
+  const faqTabPane = document.getElementById(faqTabPaneId);
+  const faqEvtButton = evt.target;
+  const faqButtonActive = faqButtonsContainer.querySelector('.faq__tab-button--active');
+  const faqTabPaneActiveId = document.getElementById(faqButtonActive.dataset.tab);
+
+  faqButtonActive.classList.remove('faq__tab-button--active');
+  faqTabPaneActiveId.classList.remove('faq__tab-pane--active');
+
+  if (faqEvtButton.className === 'faq__tab-button faq__tab-button--active') {
+    faqEvtButton.classList.remove('faq__tab-button--active');
+    faqTabPane.classList.remove('faq__tab-pane--active');
+  } else {
+    faqEvtButton.classList.add('faq__tab-button--active');
+    faqTabPane.classList.add('faq__tab-pane--active');
+  }
+});
+
+// нажатие кнопки + для открытия ответа на вопрос
+faqTabItems.forEach((faqTabItem) => {
+  const question = faqTabItem.querySelector("h3");
+
+  question.addEventListener("click", () => {
+    if (faqTabItem.classList.contains("faq__tab-item--active")) {
+      faqTabItem.classList.remove("faq__tab-item--active");
+    } else {
+      faqTabItem.classList.add("faq__tab-item--active");
+    }
+  });
+})
+
